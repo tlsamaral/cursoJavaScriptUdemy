883d90e5670c6a0f5304d88a8e18d02de9d608e2
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2023-01-24 23:31:48 -0300

    Foco no objetivo!

---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Bom dia'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo  = 'Ate logo!'
---
+++ node/moduloCliente.js
@@ -0,0 +1,10 @@
+const moduloA = require('./moduloA')//chamada para o modulo criado
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomdia)
+console.log(moduloB.boaNoite())
